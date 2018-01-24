60d249b9ba1f3bc652b82768ed23e78950e576b5
Ryan Sabo <sabora920@gmail.com>
2018-01-24 12:48:42 -0500

    finished the push and pop methods

--- array.js
+++ array.js
@@ -4,23 +4,53 @@ const mem = new Memory();
 
 
 class Array {
-  constructor(initialCapacity) {
+  constructor() {
     this.length = 0;
-    this.capacity = initialCapacity;
+    this._capacity = 0;
     this.ptr = mem.allocate(this.length);
   }
   //cannot preallocate slots using this constructor
   
-  // push(value) {
-  //   if (this,length >= this.capacity) {
-  //     this._resize((this.lengh))
-  //   }
-  // }
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+    mem.set(this.ptr + this.length, value);
+    this.length++;
+  }
 
+  _resize(size){
+    const oldPtr = this.ptr;
+    this.ptr = mem.allocate(size);
+    if(this.ptr === null){
+      throw new Error('Out of Memory')
+    }
+    mem.copy(this.ptr, oldPtr, this.length);
+    mem.free(oldPtr);
+    this._capacity = size;
+  }
 }
+Array.SIZE_RATIO = 3;
 
-let myArr = new Array(20);
-// myArr = [1, 2, 3, 4];
+let myArr = new Array();
+myArr = [1, 2, 3, 4];
 
 
-console.log(myArr);
+function main(){
+  
+      Array.SIZE_RATIO = 3;
+  
+      //create an instance of the array class
+      let arr = new Array();
+  
+      //add an item to the array
+      arr.push(10);
+      arr.push(5);
+      arr.push(15);
+      arr.push(19);
+      arr.push(45);
+      arr.push(10);
+  
+      console.log(arr);
+  }
+  main();
